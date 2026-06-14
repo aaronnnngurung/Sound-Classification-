1347d13819f51dee5ae0d35c45cff2bf4e43cd01
aaronnnngurung <aarongurung3@gmail.com>
2026-06-15 00:26:04 +0545

    saved latest homepage and authentication changes

--- flutter_application_1/lib/homepage.dart
+++ flutter_application_1/lib/homepage.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   State<HomePage> createState() => _HomePageState();
