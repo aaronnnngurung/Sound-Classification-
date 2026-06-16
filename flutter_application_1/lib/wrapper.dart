43d3c283dd6a21250427d5e201a7befde0c69f5b
aaronnnngurung <aarongurung3@gmail.com>
2026-06-17 01:57:53 +0545

    Working forgot password

--- flutter_application_1/lib/wrapper.dart
+++ flutter_application_1/lib/wrapper.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'login.dart';
 import 'homepage.dart';
+import 'login.dart';
  
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
