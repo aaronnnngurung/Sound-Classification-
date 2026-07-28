e47527dfdca93615971ad316b4293246abbb0014
Rishika Maharjan <rishikamrz33@gmail.com>
2026-07-28 09:01:50 +0545

    Fixed release build issue by  force compileSdk 36 for wear os plugin

--- flutter_application_1/android/build.gradle.kts
+++ flutter_application_1/android/build.gradle.kts
@@ -21,6 +21,7 @@ subprojects {
 
     afterEvaluate {
         extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.apply {
+            compileSdkVersion(36)
             compileOptions {
                 sourceCompatibility = JavaVersion.VERSION_17
                 targetCompatibility = JavaVersion.VERSION_17
